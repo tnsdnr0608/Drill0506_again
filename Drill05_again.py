65a34b32f5bb5a30fee8ee431014e2884bdbd58b
Hong Soon Uk <tnsdnr0608@naver.com>
2023-10-03 02:27:29 +0900

    기본 code refactoring 완료
    (함수로 구조화시킴)

--- Drill05_again.py
+++ Drill05_again.py
@@ -8,12 +8,13 @@ from pico2d import *
 # 코드가 길어지거나 재사용의 필요성이 생기면 함수를 만든다.
 # 변수 이름, 함수 이름 구체적 작성
 
-
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
-TUK_ground = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
+
+def load_resources():
+    global TUK_ground, character
+    TUK_ground = load_image('TUK_GROUND.png')
+    character = load_image('animation_sheet.png')
 
 
 def handle_events():
@@ -30,23 +31,33 @@ def handle_events():
     pass
 
 
+def reset_world():
+    global running, x, y, frame
+    running = True
+    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    frame = 0
 
-running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
-hide_cursor()
 
-while running:
+def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
+
+
+def update_world():
+    global frame
     frame = (frame + 1) % 8
 
-    handle_events()
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+hide_cursor()
+load_resources()
+reset_world()  # 초기화
+
+while running:
+    render_world()  # world의 현재 내용을 그린다.
+    handle_events()  # 사용자 입력을 받아들인다.
+    update_world()  # world안의 객체들의 상호작용을 계산하고 그 결과를 update 한다.
 
 close_canvas()
-
-
-
-
