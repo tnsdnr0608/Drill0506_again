e2274e1ca30314b1b97f27ab01a997e6fa192b43
Hong Soon Uk <tnsdnr0608@naver.com>
2023-10-03 03:18:37 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. t 값이 1을 넘으면 다시 새로운 손 화살표가 랜덤한 랜덤 위치에 만들어짐

--- Drill05_again.py
+++ Drill05_again.py
@@ -1,5 +1,5 @@
 from pico2d import *
-import random # 랜덤 기능
+import random  # 랜덤 기능
 
 # 코딩 요령
 # 먼저 전체적인 뼈대 제작 후 세부적인 내용
@@ -44,10 +44,16 @@ def reset_world():
     frame = 0
     action = 3
 
-    sx, sy = cx, cy # p1 : 시작점
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy  # p1 : 시작점
     # hx, hy = 50, 50
-    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT) # p2: 끝점
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2: 끝점
     t = 0.0
+
 
 def render_world():
     clear_canvas()
@@ -66,11 +72,13 @@ def update_world():
     frame = (frame + 1) % 8
     action = 1 if cx < hx else 0
 
-
     if t <= 1.0:
-        cx = (1 - t) * sx + t * hx # cx는 시작 x와 와 끝 x 를 1-t:t의 비율로 섞은 위치
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x와 와 끝 x 를 1-t:t의 비율로 섞은 위치
         cy = (1 - t) * sy + t * hy
-        t += 0.001
+        t += 0.1
+    else:
+        cx, cy = hx, hy # 캐릭터 위치를 목적지 위치와 정확히 일치시킴.
+        set_new_target_arrow()
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
@@ -79,7 +87,7 @@ reset_world()  # 초기화
 
 while running:
     render_world()  # world의 현재 내용을 그린다.
-    handle_events()  # 사용자 입력을 받아들인다.
+    handle_events() # 사용자 입력을 받아들인다.
     update_world()  # world안의 객체들의 상호작용을 계산하고 그 결과를 update 한다.
 
 close_canvas()
