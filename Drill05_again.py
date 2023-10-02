8db2ffc98f24d04be4552da7e3c0bc64314d502c
Hong Soon Uk <tnsdnr0608@naver.com>
2023-10-03 02:03:46 +0900

    add 코딩 요령

--- Drill05_again.py
+++ Drill05_again.py
@@ -1,5 +1,14 @@
 from pico2d import *
 
+# 코딩 요령
+# 먼저 전체적인 뼈대 제작 후 세부적인 내용
+# 테스트 리드 타임을 줄인다.
+# 조금씩 살을 붙여감.
+# 조그마한 진전이 있으면 commit 한다.
+# 코드가 길어지거나 재사용의 필요성이 생기면 함수를 만든다.
+# 변수 이름, 함수 이름 구체적 작성
+
+
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
